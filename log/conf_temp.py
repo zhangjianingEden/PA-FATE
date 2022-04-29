6bfaa849d0702dac64304f10692ba32e5244d937
EdenZhang <82367654+zhangjianingEden@users.noreply.github.com>
2022-04-29 21:32:45 +0800

    Update conf_temp.py

--- log/conf_temp.py
+++ log/conf_temp.py
@@ -1,5 +1,5 @@
 from util import *
 
 LOG_CONF = {
-    'log_root_path': '/' + os.path.join(*__file__.split('/')[:-2], 'results'),
+    'log_root_path': '/' + os.path.join(*os.path.abspath(__file__).split('/')[:-2], 'results'),
 }
